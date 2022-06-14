9d1a8175fb658794ab1b448239858b6c9129d724
Corey Ellis <coreypozzatiellis@gmail.com>
2022-06-14 18:40:54 +1000

    cleanup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
 
 puts "Clearing Movies database..."
 Movie.destroy_all
 puts "Movies database destroyed"
 puts "Generating new Movies database"
 
-40.times do
-  movie = Movie.create!(
-    title: Faker::Movie.unique.title,
-    overview: Faker::Lorem.paragraph,
-    poster_url: Faker::LoremFlickr.image(search_terms: ['movies']),
+# 40.times do
+#   movie = Movie.create!(
+#     title: Faker::Movie.unique.title,
+#     overview: Faker::Lorem.paragraph,
+#     poster_url: Faker::LoremFlickr.image(search_terms: ['movies']),
+#     rating: rand(0..10)
+#   )
+#   puts "Added: #{movie.id}. #{movie.title}!"
+# end
+
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
+# p movies["results"][1..5]
+
+
+movies["results"][1..20].each do |movie|
+  new_movie = Movie.create!(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}",
     rating: rand(0..10)
   )
-  puts "Added: #{movie.id}. #{movie.title}!"
+  puts "Added: #{new_movie.id}. #{new_movie.title}!"
 end
